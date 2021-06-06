635d2da3df0347d706f4c333af1aa6c7fb8a0ebe
Alex Kubecka <32401275+akubecka@users.noreply.github.com>
2021-06-05 20:36:06 -0400

    added lex input from file functionality

--- lex/aLex.cpp
+++ lex/aLex.cpp
@@ -1,25 +1,57 @@
 #include <stdio.h>
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
+#include <assert.h>
+#include <vector>
 #include "aLex.h"
 using namespace std;
 
 extern int yylex();
+extern FILE* yyset_in(FILE* file);
 extern int yylineno;
 extern char* yytext;
 
-int main(void){
-    int ntoken, vtoken;
+bool test(){
+    return true;
+}
 
+void lexxerCmd(){
+    int ntoken, vtoken;
     ntoken=yylex();
+    int i=0;
     while(ntoken){
         string txt = "";
-        int sz = strlen(yytext);
+        int sz = strlen(yytext);//strlen might not get correct length(needs \0)
         for(int i=0; i<sz; i++){
             txt+=yytext[i];
         }
         cout<<txt<<" : "<<ntoken<<endl;
         ntoken = yylex();
     }
+}
+
+vector<int> lexxerFile(string str){
+    vector<int> vec;
+    int ntoken, vtoken;
+    FILE *fd;
+    fd = fopen("test.txt", "r");
+    yyset_in(fd);
+    ntoken=yylex();
+    while(ntoken){
+        vec.push_back(ntoken);
+        ntoken = yylex();
+    }
+    return vec;
+}
+
+int main(void){
+    string testStr = "test";
+    vector<int> vec;
+    
+    vec = lexxerFile(testStr);//Use this on input from file
+
+    lexxerCmd();//Use this on input from cmd line and to see details printed
+
+    
     return 0;
 }
